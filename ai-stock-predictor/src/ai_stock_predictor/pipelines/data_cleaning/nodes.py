59950d040f8be3c715e8d996ec651ffd534f061a
Patryk Mikuli <patryk.mikuli@gmail.com>
2025-06-07 22:06:36 +0200

    Cleanup data in "Founded" column

--- ai-stock-predictor/src/ai_stock_predictor/pipelines/data_cleaning/nodes.py
+++ ai-stock-predictor/src/ai_stock_predictor/pipelines/data_cleaning/nodes.py
@@ -6,4 +6,7 @@ def remove_unused_columns(stocksInformationDf: pd.DataFrame, stocksHistoryDf: pd
 
     stocksHistoryDf = stocksHistoryDf.drop(columns=['Adj Close'])
 
+    stocksInformationDf['Founded'] = stocksInformationDf['Founded'].map(lambda x: x.split(' ')[0] if len(x.split(' ')) > 1 else x)
+    stocksInformationDf['Founded'] = stocksInformationDf['Founded'].map(lambda x: x.split('/')[0] if len(x.split('/')) > 1 else x)
+
     return stocksInformationDf, stocksHistoryDf
