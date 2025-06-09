6ab0e3f52c1ac605a128024675d31d08067f745a
Stefcio345 <kepskifranio@gmail.com>
2025-06-09 19:46:00 +0200

    New pipeline registry

--- ai-stock-predictor/src/ai_stock_predictor/pipeline_registry.py
+++ ai-stock-predictor/src/ai_stock_predictor/pipeline_registry.py
@@ -1,9 +1,9 @@
 from kedro.pipeline import Pipeline
 from ai_stock_predictor.pipelines.download_data import pipeline as download_data_pipeline
 from ai_stock_predictor.pipelines.feature_engineering import pipeline as feature_engineering_pipeline
-from ai_stock_predictor.pipelines.data_modeling import pipeline as m_pipeline
 from ai_stock_predictor.pipelines.data_cleaning import pipeline as data_cleaning_pipeline
 from ai_stock_predictor.pipelines.data_merge import pipeline as data_merge_pipeline
+from ai_stock_predictor.pipelines.model_training import pipeline as model_training_pipeline  # ✅ NEW
 
 def register_pipelines() -> dict[str, Pipeline]:
     return {
@@ -11,6 +11,13 @@ def register_pipelines() -> dict[str, Pipeline]:
         "data_cleaning": data_cleaning_pipeline.create_pipeline(),
         "data_merge": data_merge_pipeline.create_pipeline(),
         "feature_engineering": feature_engineering_pipeline.create_pipeline(),
-        # "model": m_pipeline.create_pipeline(),
-        "__default__": download_data_pipeline.create_pipeline() + data_cleaning_pipeline.create_pipeline() + data_merge_pipeline.create_pipeline() + feature_engineering_pipeline.create_pipeline() # + m_pipeline.create_pipeline()
+        "model_training": model_training_pipeline.create_pipeline(),
+
+        "__default__": (
+            download_data_pipeline.create_pipeline()
+            + data_cleaning_pipeline.create_pipeline()
+            + data_merge_pipeline.create_pipeline()
+            + feature_engineering_pipeline.create_pipeline()
+            + model_training_pipeline.create_pipeline()
+        )
     }
